dcfd4d2749de1d95d6965fccce0ebae628b29e01
Priya Singh <priyasingh42613@gmail.com>
2024-03-04 22:26:38 +0530

    Created container and set its bounds

--- Project/src/HangmanGame.java
+++ Project/src/HangmanGame.java
@@ -1,3 +1,20 @@
+import java.awt.Container;
+import javax.swing.JFrame;
+class MyFrame extends JFrame{
+    Container c;
+    MyFrame(){
+        setTitle("Events on Combpbox");
+        setBounds(100, 100, 700, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        c = getContentPane();
+        c.setLayout(null);
+        
+    }
+}
 public class HangmanGame {
-    
+    public static void main(String[] args) {
+        
+    }    
 }
